a4ce748dc9cc46ac1fe77b0a533c3898f1418120
pKentschke <pia.kentschke@stud.uni-heidelberg.de>
2025-07-01 14:39:49 +0200

    corrrect functions

--- functions.py
+++ functions.py
@@ -98,20 +98,13 @@ def call_data_clean(p_threshold=None, qc_thresholds=None, normalization=None):
     elif normalization == "none":
         # no normalization
         pass
-    
-    # additional df for clustering
-    QC_info = QC_metrics[['CellType', 'Lineage', 'CellFamily', 'Organ']]
-    ATAC_seq_T.index.name = 'CellType'
-    ATAC_seq_T_reset = ATAC_seq_T.reset_index()
-    ATAC_w_info = ATAC_seq_T_reset.merge(QC_info, on='CellType', how='left')
-    ATAC_w_info = ATAC_w_info.drop_duplicates(subset='CellType', keep='first').reset_index(drop=True)
+
 
     data = {
         'ATAC_seq': ATAC_seq,
         'ATAC_seq_T': ATAC_seq_T,
         'ATAC_seq_only_scores': ATAC_seq_only_scores,
         'norm': ATAC_seq_only_scores_norm,
-        'ATAC_seq_w_info': ATAC_w_info,
         'RNA_seq': RNA_seq,
         'QC_metrics': QC_metrics,
         'exons': exons,
